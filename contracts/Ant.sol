69e9a763c62c956372fcad9becc8b7978fa5d2a4
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-28 19:33:15 +0100

    add ant erc20 token

---
+++ contracts/Ant.sol
@@ -0,0 +1,21 @@
+/// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.6.8;
+
+///@title AntsReview
+///@author Nazzareno Massari
+///@notice Ant ERC20 Token
+///@dev All function calls are currently implemented without side effecs through TDD approach
+///@dev OpenZeppelin library is used for secure contract development
+
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+
+
+contract Ant is ERC20PresetMinterPauser {
+
+  constructor()
+  ERC20PresetMinterPauser("Ant", "ANT")
+  public {
+
+  }
+
+}
