258d51a89a61fbf853c8337fe271007bb9fdf414
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-28 19:50:26 +0100

    update contract migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
+var Ant = artifacts.require("Ant");
 var AntsReview = artifacts.require("AntsReview");
 
-module.exports = function(deployer) { 
-	deployer.deploy(AntsReview); 
+module.exports = function(deployer) {
+
+	deployer.deploy(Ant).then(function() {
+		return deployer.deploy(AntsReview, Ant.address);
+	});
 };
