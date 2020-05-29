9aa612f31a8e0d535751a97b20c1dec87ba11052
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-05-29 18:35:18 +0100

    update AntsReview.sol, add peer_reviewers

--- contracts/AntsReview.sol
+++ contracts/AntsReview.sol
@@ -30,30 +30,30 @@ contract AntsReview is AntsReviewRoles {
   /// Storage
   AntReview[] public antreviews ;
 
-  mapping(uint256 => Fulfillment[]) fulfillments;
+  mapping(uint256 => Peer_Review[]) peer_reviews;
 
   /// Structs
   struct AntReview {
       address payable issuer;
       uint256 deadline;
-      string data;
+      string ipfs_hash;
       AntReviewStatus status;
       uint256 amount; //in wei
   }
 
-  struct Fulfillment {
+  struct Peer_Review {
       bool accepted;
-      address payable fulfiller;
-      string data;
+      address payable peer_reviewer;
+      string ipfs_hash;
   }
 
 
   // Events
 
-  event AntReviewIssued(uint256 antReview_id, address issuer, uint256 amount, string data);
-  event AntReviewFulfilled(uint256 antReview_id, address fulfiller, uint256 fulfillment_id, string data);
-  event FulfillmentAccepted(uint256 _antReviewId, address issuer, address fulfiller, uint256 indexed fulfillment_id, uint256 amount);
-  event AntReviewCancelled(uint256 indexed antReview_id, address indexed issuer, uint256 amount);
+  event AntReviewIssued(uint256 antReviewId, address issuer, uint256 amount, string ipfsHash);
+  event AntReviewFulfilled(uint256 antReviewId, address peer_reviewer, uint256 peerReviewId, string ipfsHash);
+  event PeerReviewAccepted(uint256 antReviewId, address issuer, address peer_reviewer, uint256 indexed peerReviewId, uint256 amount);
+  event AntReviewCancelled(uint256 indexed antReviewId, address indexed issuer, uint256 amount);
 
   constructor(address ant_) public {
     ant = AntToken(ant_);
@@ -81,8 +81,8 @@ contract AntsReview is AntsReviewRoles {
     _;
   }
 
-  modifier fulfillmentExists(uint256 _antReviewId, uint256 _fulfillmentId){
-    require(_fulfillmentId < fulfillments[_antReviewId].length);
+  modifier peerReviewExists(uint256 _antReviewId, uint256 _peerReviewId){
+    require(_peerReviewId < peer_reviews[_antReviewId].length);
     _;
   }
 
@@ -91,8 +91,8 @@ contract AntsReview is AntsReviewRoles {
     _;
   }
 
-  modifier fulfillmentNotYetAccepted(uint256 _antReviewId, uint256 _fulfillmentId) {
-    require(fulfillments[_antReviewId][_fulfillmentId].accepted == false);
+  modifier peerReviewNotYetAccepted(uint256 _antReviewId, uint256 _peerReviewId) {
+    require(peer_reviews[_antReviewId][_peerReviewId].accepted == false);
     _;
   }
 
@@ -109,10 +109,10 @@ contract AntsReview is AntsReviewRoles {
   /**
   * @dev issueAntReview(): instantiates a new bounty
   * @param _deadline the unix timestamp after which fulfillments will no longer be accepted
-  * @param _data the requirements of the antReview
+  * @param _ipfsHash the requirements of the antReview
   */
   function issueAntReview(
-      string calldata _data,
+      string calldata _ipfsHash,
       uint64 _deadline
   )
       external
@@ -123,17 +123,17 @@ contract AntsReview is AntsReviewRoles {
       whenNotPaused()
       returns (uint256)
   {
-      antreviews.push(AntReview(msg.sender, _deadline, _data, AntReviewStatus.CREATED, msg.value));
-      emit AntReviewIssued(antreviews.length.sub(1),msg.sender, msg.value, _data);
+      antreviews.push(AntReview(msg.sender, _deadline, _ipfsHash, AntReviewStatus.CREATED, msg.value));
+      emit AntReviewIssued(antreviews.length.sub(1),msg.sender, msg.value, _ipfsHash);
       return (antreviews.length.sub(1));
   }
 
   /**
   * @dev fulfillAntReview(): submit a fulfillment for the given antReview
   * @param _antReviewId the index of the antReview to be fufilled
-  * @param _data the ipfs hash which contains evidence of the fufillment
+  * @param _ipfsHash the ipfs hash which contains evidence of the fufillment
   */
-  function fulfillAntReview(uint256 _antReviewId, string memory _data)
+  function fulfillAntReview(uint256 _antReviewId, string memory _ipfsHash)
     public
     antReviewExists(_antReviewId)
     onlyPeerReviewer()
@@ -141,32 +141,32 @@ contract AntsReview is AntsReviewRoles {
     isBeforeDeadline(_antReviewId)
     whenNotPaused()
   {
-    fulfillments[_antReviewId].push(Fulfillment(false, msg.sender, _data));
-    emit AntReviewFulfilled(_antReviewId, msg.sender, (fulfillments[_antReviewId].length.sub(1)),_data);
+    peer_reviews[_antReviewId].push(Peer_Review(false, msg.sender, _ipfsHash));
+    emit AntReviewFulfilled(_antReviewId, msg.sender, (peer_reviews[_antReviewId].length.sub(1)),_ipfsHash);
   }
 
   /**
-  * @dev acceptFulfillment(): accept a given fulfillment
+  * @dev acceptPeerReview(): accept a given Peer-Review
   * @param _antReviewId the index of the antReview
-  * @param _fulfillmentId the index of the fulfillment being accepted
+  * @param _peerReviewId the index of the fulfillment being accepted
   */
-  function acceptFulfillment(uint256 _antReviewId, uint256 _fulfillmentId)
+  function acceptPeerReview(uint256 _antReviewId, uint256 _peerReviewId)
       public
       antReviewExists(_antReviewId)
-      fulfillmentExists(_antReviewId,_fulfillmentId)
+      peerReviewExists(_antReviewId,_peerReviewId)
       onlyIssuer()
       hasStatus(_antReviewId, AntReviewStatus.CREATED)
-      fulfillmentNotYetAccepted(_antReviewId, _fulfillmentId)
+      peerReviewNotYetAccepted(_antReviewId, _peerReviewId)
       whenNotPaused()
   {
-      fulfillments[_antReviewId][_fulfillmentId].accepted = true;
+      peer_reviews[_antReviewId][_peerReviewId].accepted = true;
       antreviews[_antReviewId].status = AntReviewStatus.ACCEPTED;
-      fulfillments[_antReviewId][_fulfillmentId].fulfiller.sendValue(antreviews[_antReviewId].amount);
-      emit FulfillmentAccepted(
+      peer_reviews[_antReviewId][_peerReviewId].peer_reviewer.sendValue(antreviews[_antReviewId].amount);
+      emit PeerReviewAccepted(
         _antReviewId,
         antreviews[_antReviewId].issuer,
-        fulfillments[_antReviewId][_fulfillmentId].fulfiller,
-        _fulfillmentId, antreviews[_antReviewId].amount
+        peer_reviews[_antReviewId][_peerReviewId].peer_reviewer,
+        _peerReviewId, antreviews[_antReviewId].amount
       );
   }
 
