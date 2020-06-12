b7b97144a6e74e9c82882b2f205f1e882c95e983
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-06-12 22:40:30 +0100

    Update Ant.sol

--- contracts/Ant.sol
+++ contracts/Ant.sol
@@ -4,7 +4,7 @@ pragma solidity 0.6.9;
 ///@title Ants-Review
 ///@author Nazzareno Massari
 ///@notice Ant ERC20 Token
-///@dev All function calls are currently implemented without side effecs through TDD approach
+///@dev All function calls are currently implemented without side effects through TDD approach
 ///@dev OpenZeppelin library is used for secure contract development
 
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
