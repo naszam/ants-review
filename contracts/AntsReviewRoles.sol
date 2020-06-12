5138e01d8bf9eb2458b68d9ff708e9f00e3bde97
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-06-12 22:40:45 +0100

    Update AntsReviewRoles.sol

--- contracts/AntsReviewRoles.sol
+++ contracts/AntsReviewRoles.sol
@@ -4,7 +4,7 @@ pragma solidity 0.6.9;
 ///@title Ants-Review
 ///@author Nazzareno Massari
 ///@notice AntsReviewRoles Access Management for Issuer and Peer-Reviewer
-///@dev All function calls are currently implemented without side effecs through TDD approach
+///@dev All function calls are currently implemented without side effects through TDD approach
 ///@dev OpenZeppelin library is used for secure contract development
 
 import "@openzeppelin/contracts/access/Ownable.sol";
