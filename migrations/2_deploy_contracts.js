4e6a9a41bc6046d4ff7ce71b5ebb60d1f696e2b8
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-07-08 16:17:44 +0100

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,9 +4,10 @@ var AntFaucet = artifacts.require("AntFaucet");
 
 module.exports = function(deployer) {
 
-	deployer.deploy(Ant).then(function() {
-		return deployer.deploy(AntsReview, Ant.address);
-	}).then(function() {
-		return deployer.deploy(AntFaucet, Ant.address);
-	});
+ 		deployer.deploy(Ant).then(function() {
+			return deployer.deploy(AntsReview, Ant.address);
+		}).then(function() {
+			return deployer.deploy(AntFaucet, Ant.address);
+		});
+
 };
