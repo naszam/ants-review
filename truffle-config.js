1a667b96c0e419877b6b5c6efe858421ab2d3a0e
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-08-01 20:12:23 +0100

    update truffle-config.js, kovan url

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
-// const infuraURL = "https://rinkeby.infura.io/v3/...";
+// const infuraURL = "https://kovan.infura.io/v3/";
 
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
